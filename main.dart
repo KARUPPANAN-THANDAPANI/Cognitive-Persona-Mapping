2fcdded452de555b485c884704d13d4264b69836
MONIMOON3 <mahaalatchumirajasekaran369@gmail.com>
2025-10-22 16:33:01 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,30 +1,33 @@
+
 import 'package:flutter/material.dart';
-import 'package:chat_bot/oceantest/quiz_screen.dart';
-import 'package:chat_bot/Pages/StressReliefScreen.dart';
 import 'package:chat_bot/Pages/consent_screen.dart';
+import 'package:chat_bot/Pages/animated_chat_screen.dart';
+import 'package:chat_bot/Pages/StressReliefScreen.dart';
+import 'package:chat_bot/oceantest/quiz_screen.dart';
+import 'package:chat_bot/Pages/animated_chat_screen.dart'; 
 
 void main() {
-  runApp(const OceanQuizApp());
+  runApp(const MindBotApp());
 }
 
-class OceanQuizApp extends StatelessWidget {
-  const OceanQuizApp({super.key});
+class MindBotApp extends StatelessWidget {
+  const MindBotApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'OCEAN Quiz',
+      title: 'MindBot',
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.blue,
         fontFamily: 'Roboto',
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(fontSize: 18, color: Colors.white),
-        ),
       ),
-      home: const ConsentScreen(), // Changed to show ConsentScreen first
+      home: const ConsentScreen(),
       routes: {
+        '/consent': (context) => const ConsentScreen(),
+        '/chatbot': (context) => const AnimatedChatScreen(),
         '/stress-relief': (context) => const StressReliefScreen(),
+        '/quiz': (context) => const QuizScreen(),
       },
     );
   }
